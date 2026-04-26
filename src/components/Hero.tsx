45bfd0af933235f78a83cfb6da8889b7d5c744b4
Carlos Eduardo <carlosermenezes@gmail.com>
2026-04-26 15:44:55 -0300

    design: remove badge de material e certificado do hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -66,11 +66,7 @@ const Hero = () => {
           </div>
         </div>
 
-        <p className="mt-8 text-gray-300 text-sm sm:text-base">
-          <span className="inline-block bg-teal-500 bg-opacity-20 px-4 py-2 rounded-full border border-teal-400 border-opacity-30">
-            ✓ Material Didático Incluso &nbsp;&nbsp; ✓ Certificado de Participação
-          </span>
-        </p>
+
       </div>
 
       <div className="absolute bottom-0 left-0 right-0">
